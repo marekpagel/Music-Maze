e5f8c3e2b9d65df006f7912b88e1dde6e48bfa54
Karl Tarbe <karulont@gmail.com>
2015-12-11 01:24:21 +0200

    Add RGB split effect

---
+++ src/AfterEffects.js
@@ -0,0 +1,35 @@
+AfterEffects = function(renderer, width, height) {
+    vShader = document.getElementById('vShader').textContent;
+    fShader = document.getElementById('fShader').textContent;
+    console.log(vShader);
+    this.renderer = renderer;
+    var material = new THREE.ShaderMaterial( {
+        uniforms: {
+            tex: { type: 't'},
+            amount: { type: 'v2'},
+        },
+        vertexShader: vShader,
+        fragmentShader: fShader
+    });
+    this.quad = new THREE.Mesh(new THREE.PlaneBufferGeometry(width,height),
+            material);
+    this.scene = new THREE.Scene();
+    this.scene.add(this.quad);
+    this.camera = new THREE.OrthographicCamera(
+                            width / - 2,
+                            width / 2,
+                            height / 2,
+                            height / - 2,
+                            -100, 100);
+    this.target = new THREE.WebGLRenderTarget(width, height, {
+        generateMipmaps: false
+    });
+}
+
+AfterEffects.prototype.render = function(scene, camera) {
+    this.renderer.render(scene, camera, this.target, true);
+    this.quad.material.uniforms.tex.value = this.target;
+    // TODO: connect this vector with music
+    this.quad.material.uniforms.amount.value = new THREE.Vector2(0.00,0.03);
+    this.renderer.render(this.scene, this.camera);
+}
