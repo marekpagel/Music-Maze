a63f8da7eb2fa14bea576305a9e8fdb8bc9f5c92
Roman Ring <inoryy@gmail.com>
2015-12-12 17:54:58 +0200

    fix connector placement positions in local vs world space; return proper coordinates

--- src/maze.js
+++ src/maze.js
@@ -37,8 +37,6 @@ Maze.prototype = {
      * Draw next iteration of the maze at a given position
      * prob is an array that decides probabilities of left, right and straight connectors (can have all 3, at least 1)
      *
-     * // TODO fix weird behaviour of left vs right wall when dealing with rotated local space
-     *
      * returns array of open connector points
      */
     next: function(position, rotation, prob) {
@@ -52,49 +50,42 @@ Maze.prototype = {
         if (leftRnd < prob[0]) {
             // Todo: place the connector at a random spot in [20,80] range
 
-            var leftWall1 = this._createLeftWall(self.len/5, [-10, 0, 4*self.len/5]);
-            var leftWall2 = this._createLeftWall(3*self.len/5, [-10, 0, self.len/5]);
+            var leftWall1 = this._createLeftWall(self.len/5, [-10, 0, -5*self.len/5]);
+            var leftWall2 = this._createLeftWall(3*self.len/5, [-10, 0, -2*self.len/5]);
             mesh.add(leftWall2);
             mesh.add(leftWall1);
 
-            //connectors.push([position[0]-self.len,position[1],position[2]+3*self.len/5]);
-            connectors.push([]);
+            connectors.push(this._getConnector(position, rotation, -4*self.len/5, halfPi));
         } else {
-            var leftWall = this._createLeftWall(self.len, [-10, 0, 40]);
+            var leftWall = this._createLeftWall(self.len, [-10, 0, -60]);
             mesh.add(leftWall);
         }
 
         var rightRnd = Math.random();
-        //rightRnd = 1;
         if (rightRnd < prob[1]) {
             // Todo: place the connector at a random spot in [20,80] range
 
-            var rightWall1 = this._createRightWall(self.len/5, [10, 0, 4*self.len/5]);
-            var rightWall2 = this._createRightWall(3*self.len/5, [10, 0, self.len/5]);
+            var rightWall1 = this._createRightWall(self.len/5, [10, 0, -5*self.len/5]);
+            var rightWall2 = this._createRightWall(3*self.len/5, [10, 0,-2*self.len/5]);
             mesh.add(rightWall1);
             mesh.add(rightWall2);
 
-            connectors.push([]);
+            connectors.push(this._getConnector(position, rotation, -4*self.len/5, -halfPi));
         } else {
-            var rightWall = this._createRightWall(self.len, [10, 0, 40]);
+            var rightWall = this._createRightWall(self.len, [10, 0, -60]);
             mesh.add(rightWall);
         }
 
         var frontRnd = Math.random();
         if (frontRnd < prob[2]) {
-            connectors.push([]);
+            connectors.push(this._getConnector(position, rotation, -5*self.len/5, 0));
         } else {
-            var backWall = this._createWall(self.width, self.width, self.wallTexture, [0, 0, -10], [0, 0, 0]);
+            var backWall = this._createWall(self.width, self.width, self.wallTexture, [0, 0, -100], [0, 0, 0]);
             mesh.add(backWall);
-
-            // awkward fix for weird positioning we have atm
-            // TODO: remove this once coordinates are properly handled
-            var backWall2 = this._createWall(self.width, self.width, self.wallTexture, [0, 0, 90], [0, 2*halfPi, 0]);
-            mesh.add(backWall2);
         }
 
-        var ceiling = this._createWall(self.width, self.len, self.ceilingTexture, [0, 10, 40], [halfPi, 0, 0]);
-        var floorM = this._createWall(self.width, self.len, self.floorTexture, [0, -10, 40], [-halfPi, 0, 0]);
+        var ceiling = this._createWall(self.width, self.len, self.ceilingTexture, [0, 10, -60], [halfPi, 0, 0]);
+        var floorM = this._createWall(self.width, self.len, self.floorTexture, [0, -10, -60], [-halfPi, 0, 0]);
 
         mesh.add(ceiling);
         mesh.add(floorM);
@@ -103,6 +94,8 @@ Maze.prototype = {
         mesh.rotation.set(rotation[0],rotation[1],rotation[2]);
 
         scene.add(mesh);
+        
+        return connectors;
     },
 
     _createLeftWall: function(width, position) {
@@ -114,7 +107,7 @@ Maze.prototype = {
     },
 
     _createWall: function(width, height, texture, position, rotation) {
-        var geometry = new THREE.PlaneGeometry(width, height, width, height);
+        var geometry = new THREE.PlaneGeometry(width, height);
         var material = new THREE.MeshLambertMaterial({ map: texture });
         var wall = new THREE.Mesh(geometry, material);
 
@@ -122,5 +115,11 @@ Maze.prototype = {
         wall.rotation.set(rotation[0], rotation[1], rotation[2]);
 
         return wall;
-    }
+    },
+    
+    _getConnector: function(position, rotation, newPosition, newAngle) {
+        var c = new THREE.Vector3(0,0,newPosition);
+        c.applyAxisAngle(new THREE.Vector3(0, 1, 0), rotation[1]).add(new THREE.Vector3(position[0], position[1], position[2]));
+        return [c.toArray(), [rotation[0], rotation[1] + newAngle, rotation[2]]];
+    }    
 };
